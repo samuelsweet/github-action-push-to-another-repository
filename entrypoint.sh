e1a81ffec0f6da8315278683547680c45d8f4e1d
samuelsweet <geek@sweetsquared.com>
2021-04-14 06:48:46 +0100

    enhance. cp > mv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,52 @@
-#!/bin/sh -l
+#!/bin/bash
 
 set -e  # if a command fails it stops the execution
-set -u  # script fails if trying to access to an undefined variable
-
 echo "Starts"
-SOURCE_DIRECTORY="$1"
-DESTINATION_GITHUB_USERNAME="$2"
-DESTINATION_REPOSITORY_NAME="$3"
-USER_EMAIL="$4"
-DESTINATION_REPOSITORY_USERNAME="$5"
-TARGET_BRANCH="$6"
-COMMIT_MESSAGE="$7"
 
-if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
-then
-  DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
-fi
+set -u  # script fails if trying to access to an undefined variable
 
 CLONE_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
-git config --global user.name "$DESTINATION_GITHUB_USERNAME"
-git clone --single-branch --branch "$TARGET_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
+git config --global user.name "$USER_NAME"
+echo "Cloning: git@github.com:$DESTINATION_GITHUB_REPOSITORY_USERNAME/$DESTINATION_GITHUB_REPOSITORY_NAME.git"
+git clone --single-branch --branch "$TARGET_BRANCH" "git@github.com:$DESTINATION_GITHUB_REPOSITORY_USERNAME/$DESTINATION_GITHUB_REPOSITORY_NAME.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
-TARGET_DIR=$(mktemp -d)
-mv "$CLONE_DIR/.git" "$TARGET_DIR"
+echo "Create destination repository folder if not existent"
+mkdir -p "$CLONE_DIR"/"$DESTINATION_REPOSITORY_FOLDER"
+
+echo "Delete destination files to handle deletions"
+rm -rf "${CLONE_DIR:?}/$DESTINATION_REPOSITORY_FOLDER/*"
 
 echo "Copying contents to git repo"
-cp -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR"
-cd "$TARGET_DIR"
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR"/"$DESTINATION_REPOSITORY_FOLDER"/
+cd "$CLONE_DIR"/"$DESTINATION_REPOSITORY_FOLDER"/
+
+echo "Delete .git, .idea and .github folder"
+rm -rf .git .github .idea
 
 echo "Files that will be pushed"
 ls -la
 
 echo "Adding git commit"
-
-ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+if [ -z "$COMMIT_MESSAGE" ]
+then
+  COMMIT_MESSAGE="Updated"
+fi
 
 git add .
 git status
 
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
-git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE
+Folder: $DESTINATION_REPOSITORY_FOLDER
+Source repository: $GITHUB_REPOSITORY
+Source branch: $GITHUB_REF
+Source commit: $GITHUB_SHA
+User email: $USER_EMAIL"
 
 echo "Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist

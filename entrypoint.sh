f044110e69a1c60c5215274f4b55a7eed4b25ef4
samuelsweet <geek@sweetsquared.com>
2021-04-14 07:04:17 +0100

    reverted clone

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "Cloning destination git repository"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 echo "Cloning: git@github.com:$DESTINATION_GITHUB_REPOSITORY_USERNAME/$DESTINATION_GITHUB_REPOSITORY_NAME.git"
-git clone --single-branch --branch "$TARGET_BRANCH" "git@github.com:$DESTINATION_GITHUB_REPOSITORY_USERNAME/$DESTINATION_GITHUB_REPOSITORY_NAME.git" "$CLONE_DIR"
+git clone --single-branch --branch "$TARGET_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
 echo "Create destination repository folder if not existent"
